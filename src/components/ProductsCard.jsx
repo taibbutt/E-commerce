943d10c28d9eb5e2fe035ffdb6c0ce4cf0f0ecc9
WhatsWrongOB <whatswrongobaid@gmail.com>
2024-09-20 17:11:06 +0000

    add wishlist page

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -2,80 +2,84 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const ProductsCard = ({product}) => {
+const ProductsCard = ({ product }) => {
   return (
     <>
-    <div className="group w-[295px] hover:shadow-x-[#00B207] hover:shadow-lg  h-[407px] border border-gray-300 rounded-xl relative hover:border-[#2C742F] ">
-      <div  >
-        <Link href={"/details"}>
-          <Image
-            className="w-[98%] object-cover"
-            width={500}
-            height={300}
-            src={product.image}
-            alt={product.name}
-          />
-        </Link>
+      <div className="group w-[295px] hover:shadow-x-[#00B207] hover:shadow-lg  h-[407px] border border-gray-300 rounded-xl relative hover:border-[#2C742F] ">
+        <div>
+          <Link href={"/details"}>
+            <Image
+              className="w-[98%] object-cover"
+              width={500}
+              height={300}
+              src={product.image}
+              alt={product.name}
+            />
+          </Link>
 
-        {/* Button view */}
-        <div className="absolute right-[10px] top-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <div className="bg-[#F2F2F2] w-[40px] h-[40px] rounded-full flex justify-center cursor-pointer items-center mb-2">
-            <Image width={20} height={20} src="/Heart.png" alt="Favorite" />
+          {/* Button view */}
+          <div className="absolute right-[10px] top-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <Link href={"/wishlist"}>
+              <div className="bg-[#F2F2F2] w-[40px] h-[40px] rounded-full flex justify-center cursor-pointer items-center mb-2">
+                <Image width={20} height={20} src="/Heart.png" alt="Favorite" />
+              </div>
+            </Link>
+            <Link href="/details">
+              <div className="bg-[#F2F2F2] w-[40px] h-[40px] rounded-full flex justify-center items-center cursor-pointer mb-2">
+                <Image width={20} height={20} src="/see.png" alt="View" />
+              </div>
+            </Link>
           </div>
-          <div className="bg-[#F2F2F2] w-[40px] h-[40px] rounded-full flex justify-center items-center cursor-pointer mb-2">
-            <Image width={20} height={20} src="/see.png" alt="View" />
-          </div>
-        </div>
 
-        <div className="flex justify-between items-center px-3 pt-7">
-          <div>
-            <p className="text-[#4D4D4D] text-[14px]">{product.name}</p>
-            <p className="text-[16px] py-1 font-medium">$ {product.price}</p>
-            <div className="flex">
-              <Image
-                width={100}
-                height={100}
-                className="w-[12px]"
-                src="/Star.png"
-                alt="Star"
-              />
-              <Image
-                width={100}
-                height={100}
-                className="w-[12px]"
-                src="/Star.png"
-                alt="Star"
-              />
-              <Image
-                width={100}
-                height={100}
-                className="w-[12px]"
-                src="/Star.png"
-                alt="Star"
-              />
-              <Image
-                width={100}
-                height={100}
-                className="w-[12px]"
-                src="/Star.png"
-                alt="Star"
-              />
-              <Image
-                width={100}
-                height={100}
-                className="w-[12px]"
-                src="/StarEmpty.png"
-                alt="Empty Star"
-              />
+          <div className="flex justify-between items-center px-3 pt-7">
+            <div>
+              <p className="text-[#4D4D4D] text-[14px]">{product.name}</p>
+              <p className="text-[16px] py-1 font-medium">$ {product.price}</p>
+              <div className="flex">
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-[12px]"
+                  src="/Star.png"
+                  alt="Star"
+                />
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-[12px]"
+                  src="/Star.png"
+                  alt="Star"
+                />
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-[12px]"
+                  src="/Star.png"
+                  alt="Star"
+                />
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-[12px]"
+                  src="/Star.png"
+                  alt="Star"
+                />
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-[12px]"
+                  src="/StarEmpty.png"
+                  alt="Empty Star"
+                />
+              </div>
             </div>
-          </div>
-          <div className="bg-[#F2F2F2] w-[40px] h-[40px] rounded-full flex justify-center items-center cursor-pointer">
-            <Image width={20} height={20} src="/bag.png" alt="Cart" />
+            <div className="bg-[#F2F2F2] w-[40px] h-[40px] rounded-full flex justify-center items-center cursor-pointer">
+              <Image width={20} height={20} src="/bag.png" alt="Cart" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
- </>
+    </>
   );
 };
 
