5a1e3605502f6dd6e7e6321d6e6987900426a665
WhatsWrongOB <whatswrongobaid@gmail.com>
2024-09-20 17:19:10 +0000

    add wishlist page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className='h-[60px] w-full flex items-center flex-wrap gap-3 md:gap-0 pl-[10px] md:pl-[190px]'>
-        <ul className='flex items-center gap-6'>
+    <nav className='h-[60px] w-full flex items-center  gap-3 md:gap-0 pl-[20px] md:pl-[190px]'>
+        <ul className='flex items-center gap-6 flex-wrap'>
           <li className="flex items-center gap-2 text-[14px]">
             Home
             <Image src={"/down.png"} width={10} height={10} alt="arrow" />
@@ -22,11 +22,11 @@ const Navbar = () => {
             Blog
             <Image src={"/down.png"} width={10} height={10} alt="arrow" />
           </li>
-          <li className="flex items-center gap-2 text-[14px]">
+          <li className="items-center gap-2 text-[14px] hidden sm:flex">
             About us
             <Image src={"/down.png"} width={10} height={10} alt="arrow" />
           </li>
-          <li className="flex items-center gap-2 text-[14px]">
+          <li className="hidden sm:flex items-center gap-2 text-[14px]">
             Contact us
             <Image src={"/down.png"} width={10} height={10} alt="arrow" />
           </li>
